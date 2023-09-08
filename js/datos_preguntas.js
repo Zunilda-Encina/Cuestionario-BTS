cdda40d6830d35147cc85e2f07bb537e9e97f31a
Zunilda <zunilda.encina02@gmail.com>
2023-09-08 11:00:54 -0300

    Mi primer commit

---
+++ js/datos_preguntas.js
@@ -0,0 +1,309 @@
+﻿/*
+Archivo: datos_preguntas.js
+Configuración de las preguntas del juego.
+*/
+
+let preguntas = [
+	 {
+		pregunta:'¿Cuál es el significado del nombre de su fandom?',
+		opciones:[
+			'Adorable Representative MC for Youth',
+			'Adorable Represent MC for Youth',
+			'Adorable Representante MC para Jóvenes'
+		],
+		imagen_src:'img/preguntas/ARMY.jpg',
+		respuesta_correcta: 'Adorable Representative MC for Youth'
+          },
+          {
+		pregunta:'¿Quíen es el maknae?',
+		opciones:[
+			'Suga',
+			'V',
+			'Jungkook'
+		],
+		imagen_src:'img/preguntas/maknae.jpg',
+		respuesta_correcta: 'Jungkook'
+	},
+        {
+		pregunta:'¿Quíen de los hyung se unió al baile de los maknaes?',
+		opciones:[
+			' J-hope',
+			' RM',
+			' Jin'
+		],
+		imagen_src:'img/preguntas/bailedelhyung.jpg',
+		respuesta_correcta: ' RM'
+       },
+       {
+		pregunta:'¿Quíen es? Adivina según la foto',
+		opciones:[
+			' Jin',
+			' Jimin',
+			' JK'
+		],
+		imagen_src:'img/preguntas/fotojk1.png',
+		respuesta_correcta: ' JK'
+	},
+        {
+		pregunta:'¿Quíen es? Adivina según la foto ',
+		opciones:[
+			' Suga',
+			' JK',
+			' Jimin'
+		],
+		imagen_src:'img/preguntas/fotojk2.png',
+		respuesta_correcta: ' JK'
+	},
+        {
+		pregunta:'¿Quíen es? Adivina según la foto ',
+		opciones:[
+			' JK',
+			' V',
+			' Suga'
+		],
+		imagen_src:'img/preguntas/fotojk3.png',
+		respuesta_correcta: ' JK'
+	},
+        {
+		pregunta:'¿Quíen es? Adivina según la foto ',
+		opciones:[
+			' Jimin',
+			' Suga',
+			' JK'
+		],
+		imagen_src:'img/preguntas/fotojk4.png',
+		respuesta_correcta: ' JK'
+	},
+        {
+		pregunta:'¿Quíen es? Adivina según la foto ',
+		opciones:[
+			' JK',
+			' Jin',
+			' Jimin'
+		],
+		imagen_src:'img/preguntas/fotojk5.png',
+		respuesta_correcta: ' JK'
+	},
+         {
+		pregunta:'¿Quíen es? Adivina según la foto ',
+		opciones:[
+			' J-hope',
+			' JK',
+			' Jin'
+		],
+		imagen_src:'img/preguntas/fotojk6.png',
+		respuesta_correcta: ' JK'
+	},
+         {
+		pregunta:'¿Quíen es? Adivina según la foto ',
+		opciones:[
+			' J-hope',
+			' RM',
+			' JK'
+		],
+		imagen_src:'img/preguntas/fotojk7.png',
+		respuesta_correcta: ' JK'
+	},
+        {
+		pregunta:'¿Quíen es? Adivina según la foto ',
+		opciones:[
+			' JK',
+			' J-hope',
+			' Jin'
+		],
+		imagen_src:'img/preguntas/fotojk8.png',
+		respuesta_correcta: ' JK'
+	},
+        {
+		pregunta:'¿Quíen es? Adivina según la foto ',
+		opciones:[
+			' Jin',
+                        ' JK',
+			' Suga'
+		],
+		imagen_src:'img/preguntas/fotojk9.png',
+		respuesta_correcta: ' JK'
+	},
+        {
+		pregunta:'¿Quíen es el que está al lado de JIMIN?',
+		opciones:[
+			' Jin',
+			' Suga',
+			' JK'
+		],
+		imagen_src:'img/preguntas/fotojk10.png',
+		respuesta_correcta: ' JK'
+	},
+        {
+		pregunta:'¿De quíen son estos ojos? Adivina según la foto',
+		opciones:[
+			' JK',
+			' Jimin',
+			' V'
+		],
+		imagen_src:'img/preguntas/ojosjk1.png',
+		respuesta_correcta: ' JK'
+	},
+        {
+		pregunta:'¿De quíen son estos ojos? Adivina según la foto',
+		opciones:[
+			' J-hope',
+			' JK',
+			' RM'
+		],
+		imagen_src:'img/preguntas/ojosjk2.png',
+		respuesta_correcta: ' JK'
+	},
+        {
+		pregunta:'¿De quíen son estos ojos? Adivina según la foto',
+		opciones:[
+			' Jimin',
+			' Suga',
+			' JK'
+		],
+		imagen_src:'img/preguntas/ojosjk3.png',
+		respuesta_correcta: ' JK'
+	},
+        {
+		pregunta:'¿Quíenes estan en la Hyung Line?',
+		opciones:[
+			'Suga, J-hope ,Jimin y Jin',
+			'J-hope ,Jimin V,y JK',
+			'RM, Jin, Suga y J-hope'
+		],
+		imagen_src:'img/preguntas/btshyungline.jpg',
+		respuesta_correcta: 'RM, Jin, Suga y J-hope'
+	},
+        {
+		pregunta:'¿Quíenes estan en la Vocal Line?',
+		opciones:[
+			'Jin, J-hope, Jimin y JK',
+			'JK, V, Jimin y Jin',
+			'RM, Suga, J-hope y Jin'
+		],
+		imagen_src:'img/preguntas/VocalLine.jpg',
+		respuesta_correcta: 'JK, V, Jimin y Jin'
+	},
+       {
+		pregunta:'¿Quíenes estan en la Maknae Line?',
+		opciones:[
+			' JK, V Y Jimin',
+			' Tae, Jimin y Jin',
+			' V, Suga y Jimin'
+		],
+		imagen_src:'img/preguntas/maknaeline.jpg',
+		respuesta_correcta: ' JK, V Y Jimin'
+	},
+	{
+		pregunta:'¿En qué año fueron por primera vez a los Billboard y en qué categoría ganarón?',
+		opciones:[
+			'Fueron en el 2016 y ganaron en la categoría de "Mejor Artista Social" ',
+			'Fueron en el 2016 y ganaron en la categoría de "Mejor Duo o Grupo" ',
+			'Fueron en el 2017 y ganaron en la categoría de "Mejor Artista Social'
+		],
+		imagen_src:'img/preguntas/billboard.jpg',
+		respuesta_correcta: 'Fueron en el 2017 y ganaron en la categoría de "Mejor Artista Social'
+	},
+	{
+		pregunta:'¿Cuál fue su primer álbum?',
+		opciones:[
+			'O!RUL8,2?',
+			' 2 Cool 4 Skool',
+			' Skool Luv Affair'
+		],
+		imagen_src:'img/preguntas/primer-album.jpg',
+		respuesta_correcta: ' 2 Cool 4 Skool'
+	},
+        {
+		pregunta:'¿Cuál es el nombre real de RM?',
+		opciones:[
+			' Kim Tae-hyung',
+			' Kim Nam-joon',
+			' Min Yoongi '
+		],
+		imagen_src:'img/preguntas/RM-NombreReal.jpg',
+		respuesta_correcta: ' Kim Nam-joon'
+	},
+        {
+		pregunta:'¿Cuál es el nombre real de Suga?',
+		opciones:[
+			' Min Yoongi',
+			' Park Ji-min',
+			' Jeon Jung-kook'
+		],
+		imagen_src:'img/preguntas/Suga-NombreReal.jpg',
+		respuesta_correcta: ' Min Yoongi'
+	},
+        {
+		pregunta:'¿Cuál es el nombre real de Jimin?',
+		opciones:[
+			' Park Ji-min',
+			' Kim Ji-min',
+			' Jeon Ji-min'
+		],
+		imagen_src:'img/preguntas/Jimin-NombreReal.jpg',
+		respuesta_correcta: ' Park Ji-min'
+	},
+        {
+		pregunta:'¿Cuál es el nombre real de JK?',
+		opciones:[
+			' Jeon Jung-kook',
+			' Park Ji-min',
+			' Jung Ho-seok'
+		],
+		imagen_src:'img/preguntas/JK-NombreReal.jpg',
+		respuesta_correcta: ' Jeon Jung-kook'
+	},
+       {
+		pregunta:'¿Cuál es el nombre real de J-hope?',
+		opciones:[
+			' Kim Nam-joon',
+			' Min Yoongi',
+			' Jung Ho-seok'
+		],
+		imagen_src:'img/preguntas/Jhope-NombreReal.jpg',
+		respuesta_correcta: ' Jung Ho-seok'
+	},
+        {
+		pregunta:'¿Cuál es el nombre real de V?',
+		opciones:[
+			' Kim Seok-jin',
+			' Kim Tae-hyung',
+			' Kim Nam-joon'
+		],
+		imagen_src:'img/preguntas/V-NombreReal.jpeg',
+		respuesta_correcta: ' Kim Tae-hyung'
+	},
+        {
+		pregunta:'¿Cuál es el nombre real de Jin?',
+		opciones:[
+			' Kim Seok-jin',
+			' Min Yoongi',
+			' Jung Ho-seok'
+		],
+		imagen_src:'img/preguntas/Jin-NombreReal.jpg',
+		respuesta_correcta: ' Kim Seok-jin'
+	},
+        {
+		pregunta:'¿Quienes estan en la Rap Line de BTS?',
+		opciones:[
+			' Jimin,J-hope y RM ',
+			' J-hope, Jin y JK ',
+			' Suga, J-hope y RM '
+		],
+		imagen_src:'img/preguntas/BtsRapLine.jpg',
+		respuesta_correcta: ' Suga, J-hope y RM '
+	},
+	{
+		pregunta:'¿Cuándo debutó el grupo?',
+		opciones:[
+			'15 de junio de 2013',
+			'13 de julio de 2013',
+			'13 de junio de 2013'
+		],
+		imagen_src:'img/preguntas/debut.jpeg',
+		respuesta_correcta: '13 de junio de 2013'
+	}
+
+];
+
---
+++ js/mi_codigo.js
@@ -0,0 +1,146 @@
+﻿/* 
+Archivo mi_codigo.js
+En este archivo programaremos el código correspondiente
+al juego de Trivia.
+ */
+
+let indice_pregunta_actual;
+let total_puntos;
+
+indice_pregunta_actual = 0;
+total_puntos = 10;
+
+const nombre_alumno = "Zunilda Encina";
+const maximo_preguntas_por_jugada = 3;
+const puntos_resultado_bien = 3;
+let resultado =""
+function mostrarResultado() {
+    	document.getElementById('pantalla-juego').classList.add("d-none");
+    	document.getElementById('pantalla-resultado').classList.remove("d-none");
+    	if(total_puntos >= puntos_resultado_bien){
+    		document.getElementById('pantalla-resultado').classList.add("bien");       
+   } else {
+   	document.getElementById('pantalla-resultado').classList.add("mal"); 
+    }
+    document.getElementById('resultado-puntos').innerHTML=total_puntos;
+}
+
+function obtenerSiguientePregunta() {
+    indice_pregunta_actual++;
+    if (indice_pregunta_actual < preguntas.length){
+    	mostrarPregunta(preguntas[indice_pregunta_actual]);
+    	return true;
+    } else{ return false};
+}
+
+function mostrarPregunta (pregunta){
+document.getElementById('pregunta-numero').innerHTML=(indice_pregunta_actual+1)+")";
+document.getElementById("pregunta-texto").innerHTML=pregunta.pregunta;
+document.getElementById("pregunta-imagen").setAttribute("src",pregunta.imagen_src)
+
+//capturamos los div de las opciones
+let divOpciones = document.querySelectorAll("#opciones div");
+let inputOpciones=document.querySelectorAll("#opciones input")
+let labalOpciones=document.querySelectorAll("#opciones label")
+
+for(let i=0; i < divOpciones.length; i++){
+    divOpciones[i].classList.remove("correcta");
+    divOpciones[i].classList.remove("erronea");
+    inputOpciones[i].checked=false;
+    inputOpciones[i].setAttribute("value", pregunta.opciones[i]);
+    labalOpciones[i].innerHTML = pregunta.opciones[i];
+}
+ }
+ function verificarPreguntaActual(){
+	let divOpciones = document.querySelectorAll("#opciones div");
+	let inputOpciones = document.querySelectorAll("#opciones input");
+
+	for(let i=0; i < inputOpciones.length; i++){
+		if (inputOpciones[i].checked) {
+			if (preguntas[indice_pregunta_actual].opciones[i] ==
+				preguntas[indice_pregunta_actual].respuesta_correcta) {
+			total_puntos++;
+			divOpciones[i].classList.add("correcta");
+		}else{
+			divOpciones[i].classList.add("erronea")
+		}
+		}
+	}
+
+}
+
+// mostrarPregunta(siguientePregunta);
+//  mostrarPregunta(Math.floor(Math.random() * 100));
+// mostrarResultado();
+
+function numeroAleatorio(){
+ return Math.floor(Math.random()*17);
+}
+function iniciarJuego(){
+    desordenarArray(preguntas);
+// Ocultar barra del menu y pantalla resultado
+document.querySelector("#pantalla-inicio").classList.add("d-none");
+
+document.querySelector("#pantalla-resultado").classList.add("d-none");
+// mostrar el header y la pantalla de juego 
+document.querySelector("#header").classList.remove("d-none");
+
+document.querySelector("#pantalla-juego").classList.remove("d-none");
+document.querySelector("#boton-verificar").classList.remove("d-none");
+
+indice_pregunta_actual=0;
+total_puntos=0;
+mostrarPregunta (preguntas[indice_pregunta_actual]);
+}
+
+// asociar el boton jugar 
+let boton_jugar=document.querySelector("#inicio-boton-jugar");
+boton_jugar .addEventListener("click" , iniciarJuego);
+function mostrarNombre(){
+let html_nombre_alumno = document.querySelector("#nombre_alumno");
+html_nombre_alumno.innerHTML = nombre_alumno;
+}
+
+//asociar el voton verificar
+let boton_verificar=document.querySelector("#boton-verificar");
+boton_verificar .addEventListener("click" , botonVerificar);
+
+// asociacmos el voton siguiente a  la funcion
+let boton_siguiente = document.getElementById('boton-siguiente');
+boton_siguiente.addEventListener("click",botonSiguiente);
+
+// asociacmos el voton siguiente a  la funcion
+let boton_volver = document.getElementById('resultado-boton-volver-a-jugar');
+boton_volver.addEventListener("click",iniciarJuego);
+
+
+// for (let x = 0; x < preguntas.length; x++) {
+//  console.log(preguntas[x].pregunta);
+//  console.log(preguntas[x].opciones);
+//  console.log("la respuesta es correctaes: " + preguntas[x].respuesta_correcta)
+// }
+
+
+function botonVerificar(){           
+	let algunaChequeada = false;
+	let inputOpciones = document.querySelectorAll("#opciones input");
+	 for(let z = 0; z < inputOpciones.length; z++ ){
+	 	if (inputOpciones[z].checked) {
+	 	   algunaChequeada = true;
+	 	   document.getElementById('boton-verificar').classList.add("d-none");
+		   document.getElementById('boton-siguiente').classList.remove("d-none");
+	 	   verificarPreguntaActual();
+	 	   break;
+	 	}
+	 }
+}
+
+
+function botonSiguiente(){
+	 document.getElementById('boton-siguiente').classList.add("d-none");
+	 if(!obtenerSiguientePregunta()){
+	 	mostrarResultado();
+	 }else{
+	 	document.getElementById('boton-verificar').classList.remove("d-none");
+	 }
+}
